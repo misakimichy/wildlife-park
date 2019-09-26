f7521315b0649a94ca47b192d27a9b946d4a0fa8
Misaki Koonce <misaki.koonce@gmail.com>
2019-09-25 22:25:04 -0700

    Console Get methods.

--- Program.cs
+++ Program.cs
@@ -12,6 +12,18 @@ namespace Wildlife
       Animal elephant = new Animal("Indian elephant", 10, "Female", "India");
       Animal polarBear = new Animal("Polar bear", 5, "Male","Alaska");
       Animal rhino = new Animal("White rhinoceros", 8, "Female", "South Africa");
+
+      List<Animal> Animals = new List<Animal>() { lion, elephant, polarBear, rhino };
+
+      Console.WriteLine("These are the list of the wildlife.");
+      foreach(Animal animal in Animals)
+      {
+        Console.WriteLine("-----------------------");
+        Console.WriteLine(animal.GetSpecies());
+        Console.WriteLine(animal.GetAge());
+        Console.WriteLine(animal.GetGender());
+        Console.WriteLine(animal.GetPlaceOfBirth());
+      }
     }
   }
 }
