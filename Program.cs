260bd1fa5048e6e959a617bbe4a0e104a1d2ab58
Misaki Koonce <misaki.koonce@gmail.com>
2019-09-25 22:28:27 -0700

    Create class and functions outside of Main()

--- Program.cs
+++ Program.cs
@@ -6,24 +6,28 @@ namespace Wildlife
 {
   class Program
   {
+      private static Animal lion = new Animal("Lion", 2, "Male", "Savanna");
+      private static Animal elephant = new Animal("Indian elephant", 10, "Female", "India");
+      private static Animal polarBear = new Animal("Polar bear", 5, "Male","Alaska");
+      private static Animal rhino = new Animal("White rhinoceros", 8, "Female", "South Africa");
+
+      public static List<Animal> Animals = new List<Animal>() { lion, elephant, polarBear, rhino };
+
+      public static void getAnimalInfo()
+      {
+        Console.WriteLine("These are the list of the wildlife.");
+        foreach(Animal animal in Animals)
+        {
+          Console.WriteLine("-----------------------");
+          Console.WriteLine("Species: " + animal.GetSpecies());
+          Console.WriteLine("Age: " + animal.GetAge());
+          Console.WriteLine("Gender: " + animal.GetGender());
+          Console.WriteLine("They are from: " + animal.GetPlaceOfBirth());
+        }
+      }
     public static void Main()
     {
-      Animal lion = new Animal("Lion", 2, "Male", "Savanna");
-      Animal elephant = new Animal("Indian elephant", 10, "Female", "India");
-      Animal polarBear = new Animal("Polar bear", 5, "Male","Alaska");
-      Animal rhino = new Animal("White rhinoceros", 8, "Female", "South Africa");
-
-      List<Animal> Animals = new List<Animal>() { lion, elephant, polarBear, rhino };
-
-      Console.WriteLine("These are the list of the wildlife.");
-      foreach(Animal animal in Animals)
-      {
-        Console.WriteLine("-----------------------");
-        Console.WriteLine(animal.GetSpecies());
-        Console.WriteLine(animal.GetAge());
-        Console.WriteLine(animal.GetGender());
-        Console.WriteLine(animal.GetPlaceOfBirth());
-      }
+      getAnimalInfo();
     }
   }
 }
