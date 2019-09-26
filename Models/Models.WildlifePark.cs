0e2745733cc7850ef120607e9c358bff0043a033
Misaki Koonce <misaki.koonce@gmail.com>
2019-09-25 21:55:50 -0700

    Add getter methods.

--- Models/Models.WildlifePark.cs
+++ Models/Models.WildlifePark.cs
@@ -4,18 +4,35 @@ namespace Wildlife.Models
 {
   class Animal
   {
-    public string Name;
+    public string Species;
     public int Age;
     public string Gender;
 
     public string PlaceOfBirth;
 
-    public Animal (string name, int age, string gender, string placeOfBirth)
+    public Animal (string species, int age, string gender, string placeOfBirth)
     {
-      Name = name;
+      Species = species;
       Age = age;
       Gender = gender;
       PlaceOfBirth = placeOfBirth;
+    }
+
+    public string GetSpecies()
+    {
+      return Species;
+    }
+    public int GetAge()
+    {
+      return Age;
+    }
+    public string GetGender()
+    {
+      return Gender;
+    }
+    public string GetPlaceOfBirth()
+    {
+      return PlaceOfBirth;
     }
   }
 }
--- Program.cs
+++ Program.cs
@@ -9,9 +9,9 @@ namespace Wildlife
     public static void Main()
     {
       Animal lion = new Animal("Lion", 2, "Male", "Savanna");
-      Animal elephant = new Animal("Elephant", 10, "Female", "India");
-      Animal polarBear = new Animal("Polar Bear", 5, "Male","Alaska");
-      Animal rhino = new Animal("White Rhinoceros", 8, "Female", "South Africa");
+      Animal elephant = new Animal("Indian elephant", 10, "Female", "India");
+      Animal polarBear = new Animal("Polar bear", 5, "Male","Alaska");
+      Animal rhino = new Animal("White rhinoceros", 8, "Female", "South Africa");
     }
   }
 }
